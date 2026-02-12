143458fefe6d56ed1c3485cae26063e3c277ba51
Elisa Piazzetti <eelisapea@gmail.com>
2026-02-12 16:28:10 +0100

    changes

--- database/PetUser.sql
+++ database/PetUser.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS PetUser (
    id UUID DEFAULT gen_random_uuid() PRIMARY KEY, -- que es UUID? es un identificador unico universal, útil para generar identificadores, en vez de usar numeros
-    petId UUID,
-    userId UUID,
+    petId UUID NOT NULL,
+    userId UUID NOT NULL,
     createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
     updatedAt timestamp DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (petId) REFERENCES Pet(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (userId) REFERENCES "user"(id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (userId) REFERENCES "user"(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE (petId, userId) -- impide que un usuario tenga la misma mascota dos veces
 )
