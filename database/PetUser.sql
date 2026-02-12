b58870c94fd6a781d9ca88395f6ba22619a326d5
Elisa Piazzetti <eelisapea@gmail.com>
2026-02-12 16:42:21 +0100

    added extraFields

--- database/PetUser.sql
+++ database/PetUser.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS PetUser (
    id UUID DEFAULT gen_random_uuid() PRIMARY KEY, -- que es UUID? es un identificador unico universal, útil para generar identificadores, en vez de usar numeros
     petId UUID NOT NULL,
     userId UUID NOT NULL,
+    extraFields TEXT,
     createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
     updatedAt timestamp DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (petId) REFERENCES Pet(id) ON DELETE CASCADE ON UPDATE CASCADE,
