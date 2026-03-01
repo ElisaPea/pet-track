aa78d4897e240d3f49bb4798751cb6f1efe059f4
Aroa98 <aroa98ar@gmail.com>
2026-03-01 20:26:47 +0100

    Conexión a base de datos

--- src/api/get-user.tsx
+++ src/api/get-user.tsx
@@ -1,3 +1,29 @@
+import { createClient } from '@supabase/supabase-js';
+import { useEffect } from 'react';
+
+
+const supabaseUrl = 'https://nxawqcahssgckzwqqskk.supabase.co';
+const supabaseAnonKey = 'sb_publishable_4PGaX7rokNzSpzDSLmIDdg_D_9_sQr8';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
 export function getUser() {
   // LOGICA DE SUPABASE
+
+
+
+}
+
+// Función para obtener la lista de centros veterinarios
+export async function getVetCenters() {
+  const { data, error } = await supabase
+    .from('VeterinaryCenter')// Nombre base de datos
+    .select('*'); 
+
+  if (error) {
+    console.error("Error al obtener centros:", error);
+    return [];
+  }
+  return data;
 }
